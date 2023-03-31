b6df8ded449306b11584bd8c78a94b08b1e92aac
Punga17 <129490180+Punga17@users.noreply.github.com>
2023-03-31 16:11:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ streamlit.text('🥣idly🥣')
 streamlit.text('🥗🥑🍞dosa 🥗🥑')
 streamlit.text(' 🐔🍞chapathi🐔 🍞')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+import pandas 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
