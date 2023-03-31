226ab471f11437d78940329269d501c759ae73f2
Punga17 <129490180+Punga17@users.noreply.github.com>
2023-03-31 16:03:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit 
 streamlit.title('My parents new Healthy dinner ')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('idly')
+streamlit.text('dosa')
+streamlit.text('chapathi')
